2bba15a1127f4f680a3deed4284cac0af1bc752f
Lloyd Arnold <lloarn@gmail.com>
2020-04-10 11:59:06 +0100

    Add main game loop

--- js/main.js
+++ js/main.js
@@ -3,3 +3,33 @@
 
 var keyboard = new Keyboard();
 var player = new Player();
+var gameNotOver = true
+
+function movement() {
+
+  if (keyboard.data[1]) {
+    player.deltas[0] -= 0.5;
+  } else if (player.deltas[0] < 0 ) {
+    player.deltas[0] += 1;
+  }
+
+  /*if (keyboard.data[2] && !player.jumping[0]) {
+    player.deltas[1] += 0.1;
+    player.jumping[0] = true
+  } */
+
+  if (keyboard.data[3]) {
+    player.deltas[0] += 0.5;
+  } else if (player.deltas[0] > 0 ) {
+    player.deltas[0] -= 1;
+  }
+
+}
+
+function main() {
+
+  while (gameNotOver) {
+    window.setTimeout(movement, 200);
+
+  }
+}
