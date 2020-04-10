cf867c223ecc27d2d9146160cbb29ae98c5ea97b
Lloyd Arnold <lloarn@gmail.com>
2020-04-10 13:04:57 +0100

    Introduce call to keyboard.GetKeyState

--- js/main.js
+++ js/main.js
@@ -7,18 +7,18 @@ var gameNotOver = true
 
 function movement() {
 
-  if (keyboard.data[1]) {
+  if (keyboard.GetKeyState('A')]) {
     player.deltas[0] -= 0.5;
   } /* else if (player.deltas[0] < 0 ) {
     player.deltas[0] += 1;
   } */
 
-  if (keyboard.data[2] && !player.jumping[0]) {
+  if (keyboard.GetKeyState('W') && !player.jumping[0]) {
     player.deltas[1] += 20;
     player.jumping[0] = true
   }
 
-  if (keyboard.data[3]) {
+  if (keyboard.GetKeyState('D')) {
     player.deltas[0] += 0.5;
   } /* else if (player.deltas[0] > 0 ) {
     player.deltas[0] -= 1;
