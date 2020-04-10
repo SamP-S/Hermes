76cbffdcd14bf32f690417d216a401d90b127cd6
TabenGH <tabentv@gmail.com>
2020-04-10 12:52:59 +0100

    Added main loop

--- js/main.js
+++ js/main.js
@@ -4,3 +4,24 @@
 var keyboard = new Keyboard();
 var player = new Player();
 var graphics = new Graphics();
+
+
+// WebGL compatibility check
+if (!renderer) {
+  let p = document.createElement("P");
+	p.innerHTML = "ERROR: WebGL is NOT supported";
+	document.body.appendChild(p);
+} else {
+  // Initiation Functions
+  // ...
+
+  // Enter main loop
+  main();
+}
+
+function main() {
+  requestAnimationFrame(graphics.render);
+
+
+  graphics.render();
+}
