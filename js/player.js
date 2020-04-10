bacec2f57ca6c941676a5b1e1c116ad243cb6502
TabenGH <tabentv@gmail.com>
2020-04-10 12:53:13 +0100

    Fix attribute properties

--- js/player.js
+++ js/player.js
@@ -1,6 +1,9 @@
 class Player {
   constructor() {
-    this.pos.x = -1;
-    this.pos.y = -1;
+    this.pos = { x: -1, y: -1 };
+  }
+
+  render() {
+    console.log("x: ", this.pos.x, " y: ". this.pos.y);
   }
 };
