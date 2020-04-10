7b439770b18ef702e2594cbf5daee60e08ed6b9e
TabenGH <tabentv@gmail.com>
2020-04-10 15:00:23 +0100

    Made camera orthographics

--- js/graphics.js
+++ js/graphics.js
@@ -1,45 +1,48 @@
+const ORTH = 2;
+var ASPECT = window.innerWidth/window.innerHeight;
+
 class Graphics {
-    constructor() {
-        this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-        this.renderer = new THREE.WebGLRenderer();
 
-        // Renderer Properties
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.setClearColor(0xffffff);
-        this.renderer.antialias = true;
-        this.renderer.domElement.style.border = "solid";
-        document.body.appendChild(this.renderer.domElement);
+  constructor() {
+      this.scene = new THREE.Scene();
+      // perspective camera ->k // this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+      this.camera = new THREE.OrthographicCamera(-ORTH * ASPECT, ORTH * ASPECT, -ORTH, ORTH, -ORTH, ORTH);
+      //this.camera.position.z = 5;
+      this.renderer = new THREE.WebGLRenderer();
 
-        // Resize Event Listener <-- ADD LATER
-        //window.addEventListener("resize", this.windowResize);
+      // Renderer Properties
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.renderer.setClearColor(0xffffff);
+      this.renderer.antialias = true;
+      this.renderer.domElement.style.border = "solid";
+      document.body.appendChild(this.renderer.domElement);
 
-        this.geometry = new THREE.BoxGeometry();
-        this.material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
-        this.cube = new THREE.Mesh(this.geometry, this.material);
-        this.scene.add(this.cube);
-        this.material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
-        this.lines = new THREE.Line(this.geometry, this.material);
-        this.scene.add(this.lines);
-    }
+      // Resize Event Listener <-- ADD LATER
+      //window.addEventListener("resize", this.windowResize);
 
-    /*
-    windowResize() {
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-    }
-    */
+      this.geometry = new THREE.BoxGeometry();
+      this.material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
+      this.cube = new THREE.Mesh(this.geometry, this.material);
+      this.scene.add(this.cube);
+      this.material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+      this.lines = new THREE.Line(this.geometry, this.material);
+      this.scene.add(this.lines);
+  }
 
-    // render/animation loop
-    render() {
-        this.camera.position.z = 5;
-        this.cube.rotation.x += 0.01;
-        this.cube.rotation.y += 0.01;
-        this.cube.rotation.z += 0.01;
-        this.lines.rotation.x += 0.01;
-        this.lines.rotation.y += 0.01;
-        this.lines.rotation.z += 0.01;
+  /*
+  windowResize() {
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+  */
 
-        this.renderer.render(this.scene, this.camera);
-    }
+  // render/animation loop
+  render() {
+      this.cube.rotation.x += 0.01;
+      this.cube.rotation.y += 0.01;
+      this.lines.rotation.x += 0.01;
+      this.lines.rotation.y += 0.01;
+
+      this.renderer.render(this.scene, this.camera);
+  }
 }
