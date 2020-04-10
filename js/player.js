bd17e220bf81fb400998fcc83a8a9aea61be3fb4
Lloyd Arnold <lloarn@gmail.com>
2020-04-10 11:58:51 +0100

    Add new attributes + move method

--- js/player.js
+++ js/player.js
@@ -1,6 +1,16 @@
 class Player {
   constructor() {
-    this.pos.x = -1;
-    this.pos.y = -1;
+    this.pos.x = 100;
+    this.pos.y = 100;
+    this.mass = 80;
+    this.jumping = [false, false];
+    this.dbljump = false;
+    this.deltas = [0, 0];
   }
+
+  move() {
+    this.pos.x += this.deltas[0];
+    this.pos.y += this.deltas[1];
+  }
+
 };
