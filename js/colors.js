15dbdee5006f8c53bc336d22fd2eead94c46de84
Lloyd Arnold <lloarn@gmail.com>
2020-04-11 11:24:53 +0100

    Add colours file

---
+++ js/colors.js
@@ -0,0 +1,15 @@
+class colours {
+  constructor(){
+    this.WHITE   = 0xffffff;
+    this.LGREY   = 0xa3a3a3;
+    this.MGREY   = 0x636363;
+    this.DGREY   = 0x4a4a4a;
+    this.BLACK   = 0x000000;
+    this.RED     = 0xff0000;
+    this.GREEN   = 0x00ff00;
+    this.BLUE    = 0x0000ff;
+    this.YELLOW  = 0xffff00;
+    this.CYAN    = 0x00ffff;
+    this.MAGENTA = 0xff00ff;
+  }
+};
