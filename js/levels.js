5b503059175b671fbcf902179a6d62ac08c9717e
TabenGH <tabentv@gmail.com>
2020-04-11 21:44:16 +0100

    Made a number of changes
    Set canvas 16:9 for easier consistent level design

---
+++ js/levels.js
@@ -0,0 +1,21 @@
+const STAGE = {
+  LEVELS : 3,
+  LENGTH : 12
+}
+
+const PROBABILITY = {
+  TRAP : 0.05,
+  LEVEL: 0.9
+}
+
+class Tile {
+  constructor() {
+
+  }
+}
+
+class Stage {
+  constructor() {
+
+  }
+}
