520bf333af204a94d727f114452f902200647f7f
TabenGH <tabentv@gmail.com>
2020-04-11 21:01:12 +0100

    Add timer class

---
+++ js/timer.js
@@ -0,0 +1,24 @@
+// timer class that measures ellapsed seconds or milliseconds
+// works by setting a start time
+// then using a "getter" to find the difference since then
+class Timer {
+  // does start the Timer but can be restarted with the start() function
+  constructor() {
+    this.startTime = Date.now();
+  }
+
+  // resets the timer to 0
+  start() {
+    reset();
+  }
+
+  // resets the timer to 0
+  reset() {
+    this.startTime = Date.now();
+  }
+
+  getTime(isMilliseconds) {
+    if (isMilliseconds) { return Date.now() - start; }
+    else { return (Date.now() - start) / 1000; }
+  }
+}
