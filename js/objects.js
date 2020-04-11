967e381550cd963d4949deb1c998a318ae2dc9ad
Lloyd Arnold <lloarn@gmail.com>
2020-04-11 12:23:45 +0100

    Extend class attributes and move method

--- js/objects.js
+++ js/objects.js
@@ -3,14 +3,20 @@ var colours = new Colours();
 class object {
   constructor() {
       this.pos = {x: 0, y:0}; // Top left corner of shape (if rect), otherwise centre.
+      this.deltas = { dx: 0, dy: 0};
       this.dimensions = {width: 0, height: 0};
-      this.color = 0x455063;
+      this.color = colours.MGREY;
+      this.fixed_y = true;
   }
 
   draw() {
     console.log("Error, base object class cannot be drawn.");
   }
 
-
+  move() {
+    this.physics()
+    this.pos.x += this.deltas.dx;
+    this.pos.y += this.deltas.dy;
+  }
 
 };
