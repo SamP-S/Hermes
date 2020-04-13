440fbdfcb7537e02cb447342874a3b1de1210a6b
Lloyd Arnold <lloarn@gmail.com>
2020-04-13 11:30:55 +0100

    Introduce base_sprite class

--- js/objects.js
+++ js/objects.js
@@ -1,3 +1,7 @@
+var GRAVITY = 9.8;
+var AIR_RESIST = 200;
+// deltas refer to movement PER SECOND and need to be multiplied by time delta.
+
 class Base_Object {
   constructor(dimensions=[0,0], start_pos=[0,0], type="void") {
       this.pos = {x: start_pos[0], y:start_pos[1]}; // Top left corner of shape (if rect), otherwise centre.
@@ -25,14 +29,59 @@ class Base_Object {
 };
 
 class Base_Sprite extends Base_Object {
-  constructor(dimensions, start_pos, colour = COLOURS.RED, type="enemy_base"){
+  constructor(dimensions=[0,0], start_pos=[0,0], type="sprite_base", colour = COLOURS.RED,
+              lives=0, states={none: "none"}){
     super(dimensions, start_pos, type);
     this.colour = colour;
+    this.mass = mass;
+    this.colour = colour;
+    this.lives = lives;
+    this.states = states;
   }
 
   render(){
-    console.log("Still can't be rendern but will be soon");
+    console.log("Still can't be rendered -it's a base sprite u melt");
   }
+
+  physics(){
+    // f = mnu * m * g
+    this.deltas.dx *= (1.1 - (this.mass / 300)); // friction -- IMPORTANT must override if mass <= 30kg
+    // when player height is confirmed come back here and make it real-world ish
+    this.deltas.dy = (this.mass * GRAVITY - AIR_RESIST) / this.mass;
+  }
+
+  move() {
+    // sprites can't move into walls so legal move check needed
+    this.physics(legal_move)
+    this.pos.x += this.deltas.dx;
+    if (!legal_move(all_objects)){
+      this.pos.x -= this.deltas.dx;
+      this.deltas.dx = 0;
+    }
+    this.pos.y += this.deltas.dy;
+  }
+
+  // only deals with rectangular objects -- implement SAT if need to deal with more complex shapes
+    collided(object){
+      if (this.pos.x < object.pos.x + object.dimensions.width &&
+          this.pos.x + this.dimensions.width > object.pos.x &&
+          this.pos.y < object.pos.y + object.dimensions.height &&
+          this.pos.y + this.dimensions.height > object.y) {
+            return true;
+      } else{
+          return false;
+        }
+    }
+
+  // checks no collision occurs as result of move
+    legal_move(objects){
+      objects.forEach((item, i) => {
+        if (collided(item)){
+          return false
+        }
+      });
+      return true
+    }
 
 };
 
