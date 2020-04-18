f93ddb9bc27453337fb708f02e835ab76b6a9e05
Lloyd Arnold <lloarn@gmail.com>
2020-04-18 16:45:12 +0100

    Reduce deltas

--- js/objects.js
+++ js/objects.js
@@ -40,7 +40,7 @@ class Base_Object {
 
 class Base_Sprite extends Base_Object {
   constructor(dimensions=[0,0], start_pos=[0,0], type="sprite_base", colour = COLOURS.RED,
-              lives=0, states={none: "none"}, mass=100, row=0, max_deltas = [300, 300]){
+              lives=0, states={none: "none"}, mass=100, row=0, max_deltas = [200, 200]){
     super(dimensions, start_pos, type);
     this.colour = colour;
     this.mass = mass;
