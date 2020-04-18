168d010559211f8291f614949c201ad59c3eb3bb
TabenGH <tabentv@gmail.com>
2020-04-18 15:41:38 +0100

    Add material disposal

--- js/graphics.js
+++ js/graphics.js
@@ -99,6 +99,16 @@ class Test_Object {
 
 /// Garbage Collecting ///
 
+function disposeMaterial(mtrl) {
+  if (mtrl.map) mtrl.map.dispose();
+  if (mtrl.lightMap) mtrl.lightMap.dispose();
+  if (mtrl.bumpMap) mtrl.bumpMap.dispose();
+  if (mtrl.normalMap) mtrl.normalMap.dispose();
+  if (mtrl.specularMap) mtrl.specularMap.dispose();
+  if (mtrl.envMap) mtrl.envMap.dispose();
+  mtrl.dispose();
+}
+
 // Dispose of single object in array
 function disposeNode(parentObject) {
     parentObject.traverse(function (node) {
@@ -116,23 +126,11 @@ function disposeNode(parentObject) {
                 }
                 if(materialArray) {
                     materialArray.forEach(function (mtrl, idx) {
-                        if (mtrl.map) mtrl.map.dispose();
-                        if (mtrl.lightMap) mtrl.lightMap.dispose();
-                        if (mtrl.bumpMap) mtrl.bumpMap.dispose();
-                        if (mtrl.normalMap) mtrl.normalMap.dispose();
-                        if (mtrl.specularMap) mtrl.specularMap.dispose();
-                        if (mtrl.envMap) mtrl.envMap.dispose();
-                        mtrl.dispose();
+                      disposeMaterial(mtrl);
                     });
                 }
                 else {
-                    if (node.material.map) node.material.map.dispose();
-                    if (node.material.lightMap) node.material.lightMap.dispose();
-                    if (node.material.bumpMap) node.material.bumpMap.dispose();
-                    if (node.material.normalMap) node.material.normalMap.dispose();
-                    if (node.material.specularMap) node.material.specularMap.dispose();
-                    if (node.material.envMap) node.material.envMap.dispose();
-                    node.material.dispose();
+                    disposeMaterial(node.material);
                 }
             }
         }
@@ -221,12 +219,13 @@ class Graphics {
 
     // Draw Call
     this.render(scene);
-
-
+    
     /// Garbage collecting ///
     disposeHierarchy (scene, disposeNode);
     disposeNode (mesh);
-
+    disposeMaterial(material);
+    geometry.dispose();
+    scene.dispose();
   }
 
   // Simplified Draw call by passing an objects scene
