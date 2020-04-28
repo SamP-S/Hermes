0ee3b635bb05ca6ec53a08374274d9fb98212ae7
Lloyd Arnold <lloarn@gmail.com>
2020-04-28 15:16:10 +0100

    Introduce update, update health and check dead methods

--- js/sprites.js
+++ js/sprites.js
@@ -6,24 +6,32 @@ class Player extends Base_Sprite {
     dimensions = [wind_w*0.005, wind_w*0.005];
     let max_d = [wind_w*0.3, wind_w*0.3];
     super(dimensions, start_pos, "sprite_player", COLOURS.WHITE, 3,
-         {health: {time: 0, timeLim: 10^10, state: "vibin"}, jumping: false, dbljump: false}, 80, 0, max_d);
+         {health: {time: 0, time_lim: 10^10, state: "vibin"}, jumping: false, dbljump: false}, 80, 0, max_d);
     this.distance = 0.00;
+    this.wind_h = g_eng.renderer.domElement.height;
+  }
+
+  check_dead() {
+    if (this.pos.y > this.wind_h) {
+      this.lives = 0;
+    }
+    if (this.lives == 0) {
+      alert("Bruh you got dedded");
+      // gameover
+    }
   }
 
   hit() {
-    this.states.health.time    = 0;
-    this.states.health.timeLim = 2000;
-    this.states.health.state   = "hurtin";
+    this.states.health.time     = 0;
+    this.states.health.time_lim = 2000;
+    this.states.health.state    = "hurtin";
 
+    this.check_dead();
   }
 
   physics() {
     super.physics();
     if (this.deltas.dy == 0) this.states.jumping = false;   // TODO there's a very low chance of jumping being set to false mid jumping
-    if (this.lives == 0) {                                  // do we leave or go to trouble of fixing ??
-      alert("Bruh you got dedded");
-      // gameover
-    }
   }
 
   collided(object, object_offset){
@@ -31,7 +39,7 @@ class Player extends Base_Sprite {
         this.getRight() > object.getLeft() + object_offset.x &&
         this.getTop() < object.getBottom() + object_offset.y &&
         this.getBottom() > object.getTop() + object_offset.y) {
-          if (this.states.health == "vibin" && object.isTrap) {
+          if (this.states.health.state == "vibin" && object.isTrap) {
             this.hit();
           }
           return true;
@@ -40,13 +48,13 @@ class Player extends Base_Sprite {
       }
   }
 
-  move(time=0.01, all_objects=[], object_offsets=[ {x:0, y:0}] ) {
+  move(time=0.1, all_objects=[], object_offsets=[ {x:0, y:0}] ) {
 
     this.physics()
 
     this.check_max_speeds();
 
-    //this.pos.x += this.deltas.dx * time;
+    //this.pos.x += this.deltas.dx * time;    // this line is commented because stage moves horizontally ratehr than player
 
     if (!this.legal_move(all_objects, object_offsets)){
       this.pos.x -= this.deltas.dx * time;
@@ -64,25 +72,22 @@ class Player extends Base_Sprite {
 
   }
 
-
-  // checks if have collided with enemy and docks a life if has
-  check_enemies(enemies){
-    var health_state = this.states.health.state
-    if (health_state == "hurtin" || health_state == "invincible") {
-      return
+  u_health(time=0.1) {
+    this.states.health.time += time;
+    if (this.states.health.time >= this.states.health.time_lim) {
+      this.states.health.time     = 0;
+      this.states.health.time_lim = 10^10;
+      this.states.health.state    = "vibin";
     }
+  }
 
-    enemies.forEach((item, i) => {
-      if (collided(item)){
-        this.lives -= 1;
-        // bruh u need to check item.squished(this);
-        this.hit();
-        item.kill();
-      }
-    });
+  update(u_time=0.01, u_all_objects=[], u_object_offsets=[ {x:0, y:0}] ) {
+    this.move(u_time, u_all_objects, u_object_offsets);
+    this.u_health(u_time);
   }
 
   // update with accurate values later - this is template
+  // don't think we need ??
   check_row(){
     if (this.pos.y > 400){
       this.row = 0;
